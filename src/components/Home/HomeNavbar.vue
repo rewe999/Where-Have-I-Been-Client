91587b4110b8eea2b20b9e074042a2ddcad8ce4e
rewe999 <54740556+rewe999@users.noreply.github.com>
2021-01-03 14:29:24 +0100

    #29 HomePage (#31)
    * #29 HomePage
    
    * #29 - Visual Changes
    
    Co-authored-by: Agnieszka R <agnieszkaru17@onet.pl>

---
+++ src/components/Home/HomeNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col-md-12">
+    <nav class="navbar navbar-md">
+      <div class="navbar-brand textBrandNav">WHIB</div>
+      <ul class="nav">
+        <li class="nav-item">
+          <router-link class="nav-link itemsNavBarHome" to="/login">SIGN UP</router-link>
+        </li>
+        <li class="nav-item itemsNavBarHome">
+          <router-link class="nav-link" to="/register">SIGN IN</router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeNavBar",
+};
+</script>
+
+<style>
+.BtnTgl {
+  color: #fff;
+  z-index: 100;
+}
+
+@media (max-width: 470px) {
+  .textBrandNav {
+    padding-left: 90px;
+  }
+  .itemsNavBarHome {
+    padding-left: 50px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+}
+</style>
